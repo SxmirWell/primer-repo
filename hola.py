7a015fa251cd33a70227df572f9c2680bf483880
Samir_Git <ronnysamirdiegoperezlopez@gmail.com>
2024-08-18 18:31:11 -0400

    mi primer repo
    este ves el primer archivo que supo a mi primer repo : happy

---
+++ hola.py
@@ -0,0 +1,14 @@
+"faiiwilaowaijdwiiwaidjwadowajwajilsoiidwkawhwaa"
+"""hi c¿gow are you , im fine thanks , i've been good thanks godness
+and all of this is a total builtshit 
+im lit writting code in this fucking code edtior
+"""
+#wuuwhkuhkjahukagkgwjgiwfuawugfugjedjbheubhola nmi furend hiwe are you
+#wuuwhkuhkjahukagkgwjgiwfuawugfugjedjbheubhola nmi furend hiwe are you122
+#wuuwhkuhkjahukagkgwjgiwfuawugfugjedjbheubhola nmi furend hiwe are you124
+#wuuwhkuhkjahukagkgwjgiwfuawugfugjedjbheubhola nmi furend hiwe are you125
+#wuuwhkuhkjahukagkgwjgiwfuawugfugjedjbheubhola nmi furend hiwe are you126
+#wuuwhkuhkjahukagkgwjgiwfuawugfugjedjbheubhola nmi furend hiwe are you1237
+hola = "ho +2"
+print(hola)
+
